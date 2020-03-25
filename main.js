e92f3cd7fcbcf2c73817121a2f8eb2fb558ae73c
Shannon Whalen <shannon.whalen@gmail.com>
2020-03-24 22:39:12 -0400

    attribute binding and lists

--- main.js
+++ main.js
@@ -2,6 +2,17 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'product description'
+    description: 'product description',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'http://www.shannonwhalen.com',
+    inStock: true,
+    inventory: 10,
+    onSale: false,
+    details: ['80% Cotton', '20% Polyester', 'Gender-neutral'],
+    variants: [
+      { variantId: 2234, variantColor: 'green' },
+      { variantId: 2235, variantColor: 'blue' }
+    ],
+    sizes: ['small', 'medium', 'large']
   }
 });
