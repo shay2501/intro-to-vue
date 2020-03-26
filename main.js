d979632cd76fb8e976840c511d694ad09a1feaad
Shannon Whalen <shannon.whalen@gmail.com>
2020-03-26 19:19:24 -0400

    computed properties

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
+    selectedVariant: 0,
     description: 'product description',
-    image: './assets/vmSocks-green-onWhite.jpg',
     link: 'http://www.shannonwhalen.com',
-    inStock: true,
     inventory: 15,
     onSale: false,
     details: ['80% Cotton', '20% Polyester', 'Gender-neutral'],
@@ -13,12 +13,14 @@ var app = new Vue({
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green-onWhite.jpg'
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0
       }
     ],
     sizes: ['small', 'medium', 'large'],
@@ -37,8 +39,24 @@ var app = new Vue({
         this.inStock = true;
       }
     },
-    updateProduct: function(variantImage) {
-      this.image = variantImage;
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      return this.onSale
+        ? this.brand + ' ' + this.product + ' are on SALE!'
+        : '';
     }
   }
 });
