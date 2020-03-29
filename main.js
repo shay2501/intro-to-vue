23baa2434a5d5a9e3c179c40943d91093c49e915
Shannon Whalen <shannon.whalen@gmail.com>
2020-03-28 22:23:42 -0400

    product review form

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+Vue.config.devtools = true;
 Vue.component('product', {
   props: {
     premium: {
@@ -51,7 +52,7 @@ Vue.component('product', {
     >
       Remove item
     </button>
-    
+    <product-review @review-submitted="addReview" ></product-review>
   </div>
 </div>`,
   data() {
@@ -78,7 +79,8 @@ Vue.component('product', {
           variantQuantity: 20
         }
       ],
-      sizes: ['small', 'medium', 'large']
+      sizes: ['small', 'medium', 'large'],
+      reviews: []
     };
   },
   methods: {
@@ -98,6 +100,9 @@ Vue.component('product', {
     },
     updateProduct: function(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
@@ -130,6 +135,54 @@ Vue.component('product-details', {
     template: `<ul>
       <li v-for="detail in details">{{ detail }}</li>
     </ul>`
+  }
+});
+
+Vue.component('product-review', {
+  template: `<form class="review-form" @submit.prevent="onSubmit">
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+  
+  <p>
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review"></textarea>
+  </p>
+  
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+      
+  <p>
+    <input type="submit" value="Submit">  
+  </p>    
+
+</form>`,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null
+    };
+  },
+  methods: {
+    onSubmit() {
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating
+      };
+      this.$emit('review-submitted', productReview);
+      this.rating = this.name = this.review = null;
+    }
   }
 });
 
